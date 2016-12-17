de3eab70c24fca19a9543cb614af1900ca35c0be
aaron0x <aaron1126@gmail.com>
2016-12-17 22:09:34 +0800

    Rename test file.

--- tests/test_configuration.py
+++ tests/test_configuration.py
@@ -4,8 +4,8 @@ from model.configuration import WatchCondition
 from model.configuration import WatchConfigParser
 
 
-class StockConfigParserTest(unittest.TestCase):
-    def test_read_config(self):
+class WatchConfigParserTest(unittest.TestCase):
+    def test_read(self):
         wc1 = WatchCondition('1565', 0.1, 123.4)
         wc2 = WatchCondition('2727', 10.0, 88.0)
         expected_wcs = [wc1, wc2]
