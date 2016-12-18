5572d64913a2749587b5bc9fb737b84e12891a75
aaron0x <aaron1126@gmail.com>
2016-12-18 22:38:25 +0800

    Using mock.

--- tests/test_querier.py
+++ tests/test_querier.py
@@ -1,4 +1,6 @@
 import unittest
+import requests
+from mock import MagicMock
 
 from model.querier import Stock
 from model.querier import PriceQuerier
@@ -43,45 +45,40 @@ class FakeResponse(object):
     status_code = status_code
     response = response
 
-    def __init__(self):
-        self.status_code = FakeResponse.status_code
-        self.text = FakeResponse.response
+    @property
+    def status_code(self):
+        return FakeResponse.status_code
 
-
-class FakeRequest(object):
-    url = ''
-
-    def get(self, url):
-        FakeRequest.url = url
-        return FakeResponse()
-
-
-class FakeRequestFactory(object):
-    def create_request(self):
-        return FakeRequest()
+    @property
+    def text(self):
+        return FakeResponse.response
 
 
 class PriceQuerierTestCase(unittest.TestCase):
+    def setUp(self):
+        requests.get = MagicMock(return_value = FakeResponse())
+
+
     def test_query(self):
         expected_stocks = [Stock('1565.TWO', 150), Stock('2727.TW', 90.9)]
         FakeResponse.status_code = status_code
         FakeResponse.response = response
 
-        pq = PriceQuerier(FakeRequest())
+        pq = PriceQuerier(requests)
         stocks = pq.query(['1565.TWO', '2727.TW'])
-        self.assertEqual(FakeRequest.url, 'https://query.yahooapis.com/v1/public/yql?q=select LastTradePriceOnly from yahoo.finance.quote where symbol in ("1565.TWO","2727.TW")&format=json&env=store://datatables.org/alltableswithkeys&callback=')
+        requests.get.assert_called_with('https://query.yahooapis.com/v1/public/yql?q=select LastTradePriceOnly from yahoo.finance.quote where symbol in ("1565.TWO","2727.TW")&format=json&env=store://datatables.org/alltableswithkeys&callback=')
         self.assertEqual(stocks, expected_stocks)
 
     def test_query_failed(self):
         FakeResponse.status_code = 500
 
-        pq = PriceQuerier(FakeRequest())
+        pq = PriceQuerier(requests)
         stocks = pq.query(['1565.TWO', '2727.TW'])
         self.assertEqual(len(stocks), 0)
 
         FakeResponse.status_code = 200
         FakeResponse.response = error_response
 
-        pq = PriceQuerier(FakeRequest())
+        pq = PriceQuerier(requests)
         stocks = pq.query(['1565.TWO', '2727.TW'])
         self.assertEqual(len(stocks), 0)
--- tests/test_watcher.py
+++ tests/test_watcher.py
@@ -1,10 +1,11 @@
 import unittest
-
+import requests
+from mock import MagicMock
 
 from model.configuration import WatchCondition
 from model.querier import Stock
 from model.watcher import Watcher
-from test_querier import FakeRequest
+from test_querier import FakeResponse
 
 
 class FakeWatchConfigParser(object):
@@ -35,8 +36,9 @@ class FakeNotifier(object):
 class WatcherTestCase(unittest.TestCase):
     def test_watch(self):
         expected_message = '1565.TWO is [150], out of (0.1, 123.4)'
+        requests.get = MagicMock(return_value = FakeResponse())
 
-        w = Watcher(FakeWatchConfigParser(), FakePriceQuerier(FakeRequest()), FakeNotifier())
+        w = Watcher(FakeWatchConfigParser(), FakePriceQuerier(requests), FakeNotifier())
         w.watch('./config')
 
         self.assertEqual(FakeNotifier.message, expected_message)
