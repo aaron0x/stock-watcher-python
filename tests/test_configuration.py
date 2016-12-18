4b96ed54184fee3e263ee17c2855b82b43308aba
aaron0x <aaron1126@gmail.com>
2016-12-18 10:24:38 +0800

    Change config format.

--- tests/test_configuration.py
+++ tests/test_configuration.py
@@ -6,8 +6,8 @@ from model.configuration import WatchConfigParser
 
 class WatchConfigParserTestCase(unittest.TestCase):
     def test_read(self):
-        wc1 = WatchCondition('1565', 0.1, 123.4)
-        wc2 = WatchCondition('2727', 10.0, 88.0)
+        wc1 = WatchCondition('1565.TWO', 0.1, 123.4)
+        wc2 = WatchCondition('2727.TW', 10.0, 88.0)
         expected_wcs = [wc1, wc2]
 
         wcp = WatchConfigParser()
