b7f8d8d7015b82a717ac8afc8b8de76c084d7269
aaron0x <aaron1126@gmail.com>
2016-12-30 20:22:21 +0800

    Adjust test.

--- tests/test_notification.py
+++ tests/test_notification.py
@@ -9,9 +9,6 @@ from email.mime.text import MIMEText
 
 class NotifierTestCase(unittest.TestCase):
     def test_notify(self):
-        fake_smtp = mock.Mock()
-        n = Notifier(fake_smtp)
-
         expected_to_adds = ['aaron1126@gmail.com', 'silver@yahoo.com']
         message = 'goodday'
         expected_smtp_setting = SMTPSetting('smtp.gmail.com:587', 'aaron', 'haveaniceday', 'aaron@gmail.com', 'subject')
@@ -20,6 +17,8 @@ class NotifierTestCase(unittest.TestCase):
         expected_message['To'] = ','.join(expected_to_adds)
         expected_message['Subject'] = expected_smtp_setting.subject
 
+        fake_smtp = mock.Mock()
+        n = Notifier(fake_smtp)
         n.notify(expected_smtp_setting, expected_to_adds, message)
 
         fake_smtp.connect.assert_called_with(expected_smtp_setting.server)
--- tests/test_watcher.py
+++ tests/test_watcher.py
@@ -13,14 +13,12 @@ class WatcherTestCase(unittest.TestCase):
         expected_smtp_setting = SMTPSetting('smtp.gmail.com:587', 'aaron', 'TWRocks', 'aaron@gmai.com', 'subject')
         expected_message = '1565.TWO is [150], out of (0.1, 123.4)'
         expected_to_adds = ['aaron1126@gmail.com', 'silver@yahoo.com']
-
         wc1 = WatchCondition('1565.TWO', 0.1, 123.4)
         wc2 = WatchCondition('2727.TW', 10.0, 88.0)
         watch_config_parser = Mock(watch_conditions = [wc1, wc2], smtp_setting = expected_smtp_setting, to_addrs = expected_to_adds, query_timeout = 3)
         attrs = {'query.return_value' : [Stock('1565.TWO', 150), Stock('2727.TW', 50.9)]}
         price_querier = Mock(**attrs)
         notifier = MagicMock()
-
 
         w = Watcher(watch_config_parser, price_querier, notifier)
         w.watch()
