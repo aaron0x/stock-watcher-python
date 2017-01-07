3b7b6a56f54d8be31e6d5c2862624b5516e9a261
aaron0x <aaron1126@gmail.com>
2017-01-07 19:57:25 +0800

    Make Logger singleton.

--- model/logger.py
+++ model/logger.py
@@ -2,7 +2,13 @@ import logging
 from logging.handlers import RotatingFileHandler
 
 
+logger = None
+
 def get_logger(name, log_config):
+    global logger
+    if logger:
+        return logger
+
     logger = logging.getLogger(name)
     logger.setLevel(log_config.level)
     handler = RotatingFileHandler(log_config.path, maxBytes=log_config.max_size,
