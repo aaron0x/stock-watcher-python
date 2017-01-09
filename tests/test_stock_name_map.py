3ba290ea2f9c6de1334633eb43e521b26a93b284
aaron0x <aaron1126@gmail.com>
2017-01-09 08:26:20 +0800

    Refactory test.

--- tests/test_stock_name_map.py
+++ tests/test_stock_name_map.py
@@ -8,21 +8,21 @@ from model.name_map import NameMapper
 
 class StockNameMapperTestCase(unittest.TestCase):
     def test_map_from_repository(self):
-        stock_numbers = [u'1565.TWO']
+        numbers = [u'1565.TWO']
         expected_names = [u'精華']
         attrs = {'get_name.return_value': u'精華'}
         repository = Mock(**attrs)
         name_querier = Mock()
 
         mapper = NameMapper(repository, name_querier)
-        result = mapper.map_async(stock_numbers, 3)
+        result = mapper.map_async(numbers, 3)
 
-        name_querier.assert_not_called()
+        self.assertEqual(result.result, expected_names)
         repository.get_name.assert_called_once()
-        self.assertEqual(expected_names, result.result)
+        name_querier.assert_not_called()
 
     def test_map_from_querier(self):
-        stock_numbers = [u'1565.TWO']
+        numbers = [u'1565.TWO']
         expected_names = [u'精華']
         attrs1 = {'get_name.return_value': None}
         repository = Mock(**attrs1)
@@ -30,9 +30,10 @@ class StockNameMapperTestCase(unittest.TestCase):
         name_querier = Mock(**attrs2)
 
         mapper = NameMapper(repository, name_querier)
-        result = mapper.map_async(stock_numbers, 3)
+        result = mapper.map_async(numbers, 3)
 
+        self.assertEqual(result.result, expected_names)
         repository.get_name.assert_called_once()
-        repository.save_name.assert_called_with(u'1565.TWO', u'精華')
         name_querier.query_async.assert_called_once()
-        self.assertEqual(expected_names, result.result)
+        repository.save_name.assert_called_with(u'1565.TWO', u'精華')
+
